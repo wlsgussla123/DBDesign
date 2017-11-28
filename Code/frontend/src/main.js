c8fde7ce043872297a6442fa80787fb8a020ab95
Jinhyeon Park <wlsgussla123@gmail.com>
2017-11-28 19:29:14 +0900

    set up vue-material and axios
    design framework and http library

--- Code/frontend/src/main.js
+++ Code/frontend/src/main.js
@@ -1,10 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import axios from 'axios';
 import Vue from 'vue';
-import App from './App';
-import router from './router';
+import VueAxios from 'vue-axios';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+
+import App from './App'; // load App
+import router from './router'; // about router
+import './styles/common.scss'; // about style
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios);
+Vue.use(VueMaterial);
 
 /* eslint-disable no-new */
 new Vue({
