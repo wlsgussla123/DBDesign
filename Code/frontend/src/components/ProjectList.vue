190d48cecec0352b76938d9c16860605425c9b97
ieon <kimiron518@naver.com>
2017-12-02 22:32:13 +0900

    table module
    vue에서 쓰이는 table 형식 import

---
+++ Code/frontend/src/components/ProjectList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <my-header></my-header>
+    <main>
+        프로젝트 리스트 보기
+
+        <div id="demo">
+            <demo-grid
+                :data="gridData"
+                :columns="gridColumns"
+                :filter-key="searchQuery">
+            </demo-grid>
+        </div>
+
+
+
+
+    </main>
+    <footer></footer>
+  </section>
+</template>
+
+<script>
+import Header from './Header';
+
+export default {
+  name: 'ProjectList',
+  components: {
+    myHeader: Header,
+  },
+};
+</script>
+
+<style lang="scss" module>
+</style>
